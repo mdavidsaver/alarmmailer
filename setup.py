d92aa8f47dfe070a7dc3db30765236b6fa9ae970
Michael Davidsaver <mdavidsaver@bnl.gov>
2014-10-11 12:30:42 -0400

    create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+  name='alarmmailer',
+  version='1.0-dev',
+  description='Email alarm notifier for EPICS',
+  author = 'Michael Davidsaver',
+  author_email = 'mdavidsaver@gmail.com',
+  license = 'GPL2',
+  url = 'http://github.com/mdavidsaver/alarmmailer',
+  
+  packages = ['alarmmail', 'alarmmail.templatetags'],
+  scripts = ['alarmmailer', 'alarmmailertest'],
+  data_files = [
+    ('/etc/alarmmailer', ['mailer.conf', 'pvs.conf', 'dest.conf']),
+  ]
+)
